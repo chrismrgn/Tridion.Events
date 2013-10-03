ec805fd5efa433dcbd59208ac805eba7578c51ef
chrismrgn <chris@mrgn.co>
2013-10-03 10:10:12 -0700

    Initial Commit

---
+++ Tridion.Templates.2013.Events/EventSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tridion.ContentManager.ContentManagement;
+using Tridion.ContentManager.Extensibility;
+using Tridion.ContentManager.Extensibility.Events;
+
+namespace Tridion.Templates._2013.Events
+{
+    [TcmExtension("Tridion.Events.COMPANYNAME")] 
+    public class EventSystem : TcmExtension
+    {
+        public EventSystem()
+        {
+            Subscribe();
+        }
+        public void Subscribe()
+        {
+            // Subscribe to OnComponentSavePre
+            EventSystem.Subscribe<Component, SaveEventArgs>(OnComponentSavePre, EventPhases.Initiated);
+        }
+        private static void OnComponentSavePre(Component comp, SaveEventArgs args, EventPhases phase)
+        {
+            //Do Something...
+        }
+    }
+}
---
+++ Tridion.Templates.2013.Events/Readme.txt
@@ -0,0 +1,13 @@
+﻿In order to build this solution you will have to add references to 4 Tridion DLLs
+
+-Tridion.Common.dll
+-Tridion.ContentManager.dll
+-Tridion.ContentManager.Publishing.dll
+-Tridion.Logging.dll
+
+These can be found in the {TRIDION_HOME}/bin/client directory on your content management server.
+
+Please direct all questions to 
+
+http://tridion.stackexchange.com
+
